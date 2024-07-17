22c457b17fe9c1347cf455d2eef583707f38fd99
bartbussmann <bartbussmann@gmail.com>
2024-07-17 18:31:49 +0000

    setup sweep

--- train_script.py
+++ train_script.py
@@ -15,13 +15,16 @@ cfg["activation"] = "relu"
 cfg["tied_weights"] = False
 cfg["input_unit_norm"] = True
 cfg["perf_log_freq"] = 1000
-
-cfg["top_k"] = 64
+cfg["project"] = "batch_topk_sweep"
+cfg["top_k"] = 16
 cfg["sae_type"] = "topk"
 cfg["dict_size"] = 768 * 16
 cfg = post_init_cfg(cfg)
 
-sae = TopKSAE(cfg)
+if cfg["sae_type"] == "topk":
+    sae = TopKSAE(cfg)
+elif cfg["sae_type"] == "batchtopk":
+    sae = BatchTopKSAE(cfg)
         
 model = HookedTransformer.from_pretrained(cfg["model_name"]).to(cfg["dtype"]).to(cfg["device"])
 activations_store = ActivationsStore(model, cfg)
